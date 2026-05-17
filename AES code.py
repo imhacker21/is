850aa247a46844835ccf2242adf690960e946813
imhacker21 <kiran.chavansatara04@gmail.com>
2026-05-17 22:50:50 +0530

    Add files via upload

---
+++ AES code.py
@@ -0,0 +1,28 @@
+
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+import base64
+
+# ---------------- ENCRYPTION ----------------
+
+key = b'abcdefghijklmnop'
+message = input("Enter message: ")
+cipher = AES.new(key, AES.MODE_CBC)
+encrypted = cipher.encrypt(pad(message.encode(), AES.block_size))
+encrypted_text = base64.b64encode(cipher.iv + encrypted).decode()
+
+print("\nEncrypted Text:", encrypted_text)
+
+# ---------------- DECRYPTION ----------------
+
+data = base64.b64decode(encrypted_text)
+iv = data[:16]
+encrypted_message = data[16:]
+decrypt_cipher = AES.new(key, AES.MODE_CBC, iv)
+decrypted = unpad(
+    decrypt_cipher.decrypt(encrypted_message),
+    AES.block_size
+).decode()
+
+print("Decrypted Text:", decrypted)
+
